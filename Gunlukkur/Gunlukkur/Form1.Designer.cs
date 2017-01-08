dd085544ed4636bb222f7b5199067ef99343c4a0
metin.sen <metin.sen@ALS-YG39506-J06>
2017-01-08 18:29:49 +0300

    2

--- Gunlukkur/Gunlukkur/Form1.Designer.cs
+++ Gunlukkur/Gunlukkur/Form1.Designer.cs
@@ -30,22 +30,27 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btngetir = new System.Windows.Forms.Button();
+            this.dGridKurlar = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label2 = new System.Windows.Forms.Label();
+            this.cBoxDovizTuru = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridKurlar)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(118, 17);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // btngetir
             // 
-            this.btngetir.Location = new System.Drawing.Point(12, 12);
+            this.btngetir.Location = new System.Drawing.Point(12, 113);
             this.btngetir.Name = "btngetir";
             this.btngetir.Size = new System.Drawing.Size(75, 23);
             this.btngetir.TabIndex = 1;
@@ -54,26 +59,75 @@
             this.btngetir.UseVisualStyleBackColor = true;
             this.btngetir.Click += new System.EventHandler(this.btngetir_Click);
             // 
+            // dGridKurlar
+            // 
+            this.dGridKurlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGridKurlar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dGridKurlar.Location = new System.Drawing.Point(93, 12);
+            this.dGridKurlar.Name = "dGridKurlar";
+            this.dGridKurlar.ReadOnly = true;
+            this.dGridKurlar.Size = new System.Drawing.Size(271, 228);
+            this.dGridKurlar.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Para Birimi";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Döviz Tutarı";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(118, 41);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(9, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tarih";
+            // 
+            // cBoxDovizTuru
+            // 
+            this.cBoxDovizTuru.FormattingEnabled = true;
+            this.cBoxDovizTuru.Location = new System.Drawing.Point(12, 86);
+            this.cBoxDovizTuru.Name = "cBoxDovizTuru";
+            this.cBoxDovizTuru.Size = new System.Drawing.Size(67, 21);
+            this.cBoxDovizTuru.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Döviz türü";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(609, 386);
+            this.ClientSize = new System.Drawing.Size(376, 248);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cBoxDovizTuru);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.dGridKurlar);
             this.Controls.Add(this.btngetir);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dGridKurlar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,7 +137,12 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btngetir;
+        private System.Windows.Forms.DataGridView dGridKurlar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cBoxDovizTuru;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Gunlukkur/Gunlukkur/Form1.cs
+++ Gunlukkur/Gunlukkur/Form1.cs
@@ -18,17 +18,45 @@ namespace Gunlukkur
             InitializeComponent();
         }
 
+        Boolean Dolardurum = false, Eurodurum = false, Pounddurum = false;
         private void btngetir_Click(object sender, EventArgs e)
         {
+
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load("http://www.tcmb.gov.tr/kurlar/today.xml");
             DateTime Tarih= Convert.ToDateTime(xmlDoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
             label1.Text = Tarih.ToString("dd/MM/yyyy");
-            string USD = xmlDoc.SelectSingleNode("//Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
-            label1.Text += " - USD : " + USD;
-            string EUR = xmlDoc.SelectSingleNode("//Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
-            label2.Text = Tarih.ToString("dd/MM/yyyy");
-            label2.Text += " - EUR : " + EUR;
+
+            if (cBoxDovizTuru.SelectedItem.ToString() == "Dolar" && !Dolardurum)
+            {
+                string USD = xmlDoc.SelectSingleNode("//Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+                dGridKurlar.Rows.Add("Dolar", USD);
+                Dolardurum = true;
+            }
+            if (cBoxDovizTuru.SelectedItem.ToString() == "Euro" && !Eurodurum)
+            {
+                string EUR = xmlDoc.SelectSingleNode("//Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+                dGridKurlar.Rows.Add("Euro", EUR);
+                Eurodurum = true;
+            }
+
+            if (cBoxDovizTuru.SelectedItem.ToString() == "Pound" && !Pounddurum)
+            {
+                string GBP = xmlDoc.SelectSingleNode("//Tarih_Date/Currency[@Kod='GBP']/BanknoteSelling").InnerXml;
+                dGridKurlar.Rows.Add("Pound", GBP);
+                Pounddurum = true;
+            }
+            
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cBoxDovizTuru.Items.Add("Dolar");
+            cBoxDovizTuru.Items.Add("Euro");
+            cBoxDovizTuru.Items.Add("Pound");
         }
     }
 }
